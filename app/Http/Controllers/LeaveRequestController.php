f5f398964736d3cd1fe331a202da35846e672d65
kgssandeepa <sonalsandeepa08@gmail.com>
2025-12-09 15:43:43 +0530

    change leave request controll

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreLeaveRequest;
+use App\Http\Resources\LeaveResource;
 use Illuminate\Http\Request;
 use App\Models\LeaveRequest;
 use App\Models\Employee;
@@ -21,11 +22,12 @@ class LeaveRequestController extends Controller
        if (!$employee) {
         return response()->json(['message' => 'Employee not found'], 404);
     }
-    //dd('log');
+   // dd('log');
 
     // Reduce leave count
     if ($request->leave_type === 'annual') {
-
+        
+        dd('log');
             if ($employee->annual_leave_count == 0 ) {
         return response()->json([
             'message' => 'No annual leave remaining'
@@ -38,19 +40,21 @@ class LeaveRequestController extends Controller
 
     if ($request->leave_type === 'casual') {
         $employee->casual_leave_count -= 1;
-
+   //dd('log');
          return response()->json(['massage'=>'You are not casual leave'],400);
     }
 //dd('log');
     // Savecounts
-      $employee->save();
+     $employee->save();
     $employee = LeaveRequest::create($validated);
         //dd('log');
         return response()->json([
             
+             'data'=>new LeaveResource($employee),
             'message' => 'Leave request submitted successfully',
             'remaining_annual_leaves'=>$employee->annual_leave_count,
-            'remaining_casual_leaves'=>$employee->casual_leave_count
+            'remaining_casual_leaves'=>$employee->casual_leave_count,
+           
         
         ]);
 
